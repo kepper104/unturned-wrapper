be4ebe7a6bbcab6f42a56828fc5dddb17ed5f79d
kepper104 <67190416+kepper104@users.noreply.github.com>
2022-12-21 20:36:42 +0300

    Testing

--- main.py
+++ main.py
@@ -1,6 +1,54 @@
 import main2
 import time
 # a = main2.Interactor()
+print("""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""")
 time.sleep(20)
 main2.i.send_input("players")
 
