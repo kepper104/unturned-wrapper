b0149a6b74b51f196885891be93c0fa0a873533f
kepper104 <67190416+kepper104@users.noreply.github.com>
2022-12-21 19:57:01 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,15 +1,31 @@
 import subprocess
 
+class Interactor():
 
-def execute(cmd):
-    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
-    for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line
-    popen.stdout.close()
-    return_code = popen.wait()
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, cmd)
+    # Start the process and store the Popen object
+    def __init__(self):
+        self.popen = subprocess.Popen(["./ServerHelper.sh"], stdout=subprocess.PIPE,
+                                 stdin=subprocess.PIPE, universal_newlines=True)
+
+        # Iterate over the output of the process
+        for stdout_line in iter(self.popen.stdout.readline, ""):
+            print(stdout_line, end="")
+
+        self.popen.stdout.close()
+        return_code = self.popen.wait()
+        if return_code:
+            raise SystemError
 
 
-for path in execute(["./ServerHelper.sh"]):
-    print(path, end="")
+    def send_input(self, input_str):
+        # Send input to the process
+        self.popen.stdin.write(input_str)
+        self.popen.stdin.flush()
+
+
+    # Example usage
+    # send_input("input string\n")
+
+# Close the stdout stream and wait for the process to finish
+
+i = Interactor()
