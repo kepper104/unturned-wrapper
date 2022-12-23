a42ecfe215a26345cb42eac1651e4a5a8774cbd2
kepper104 <67190416+kepper104@users.noreply.github.com>
2022-12-23 10:33:51 +0300

    try1

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, jsonify
-from flask_socketio import SocketIO, emit
 from threading import Thread
 from time import sleep
 from os import chdir
 from expect import Interactor
-
+from turbo_flask import Turbo
 
 class ServerControl:
     def __init__(self):
         self.app = Flask(__name__)
         self.app.config['SECRET_KEY'] = 'secret!'
-        socketio = SocketIO(self.app)
         self.server_logs = ""
         # self.index_path = "/home/kepper104/hosting/unturned-wrapper/index.html"
         chdir("/home/kepper104/hosting/unturned-wrapper/")
@@ -24,23 +22,24 @@ class ServerControl:
             # self.server_logs += "hell o\n"
             return render_template(self.index_path, logs=self.server_logs)
 
-        @socketio.on('start')
+        @self.app.route("/start_server")
         def start_server():
+            print("HI")
             # Insert code here to start the server
             self.server_logs += "Starting server...\n"
-            emit('update', {'logs': self.server_logs}, broadcast=True)
+            # emit('update', {'logs': self.server_logs}, broadcast=True)
 
-        @socketio.on('stop')
-        def stop_server():
-            # Insert code here to stop the server
-            self.server_logs += "Stopping server...\n"
-            emit('update', {'logs': self.server_logs}, broadcast=True)
-
-        @socketio.on('restart')
-        def restart_server():
-            # Insert code here to restart the server
-            self.server_logs += "Restarting server...\n"
-            emit('update', {'logs': self.server_logs}, broadcast=True)
+        # @socketio.on('stop')
+        # def stop_server():
+        #     # Insert code here to stop the server
+        #     self.server_logs += "Stopping server...\n"
+        #     emit('update', {'logs': self.server_logs}, broadcast=True)
+        #
+        # @socketio.on('restart')
+        # def restart_server():
+        #     # Insert code here to restart the server
+        #     self.server_logs += "Restarting server...\n"
+        #     emit('update', {'logs': self.server_logs}, broadcast=True)
 
     def run(self):
         self.app.run(debug=False, host='0.0.0.0')
